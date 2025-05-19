7f4fe91fe4a30927f067b94fe3e4732c242775b2
anky00 <anki7876@gmail.com>
2025-05-19 19:57:52 +0530

    add code for sum

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pythonpractise
 python practise for anyone
+this repo is created for practising python code
+
+added new line
---
+++ python101.py
@@ -0,0 +1,6 @@
+# creating python script to run top command and get system info and then print same
+
+a = float(input("enter first number"))
+b =  float(input("enter first number"))
+c = a + b
+print("sum is " ,c)
